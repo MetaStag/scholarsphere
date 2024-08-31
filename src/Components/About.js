0371bc97ea35a927b887d5dc37139e404917ea2e
MetaStag <thegreek132@gmail.com>
2024-08-31 17:01:53 +0530

    update ui

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,9 +11,8 @@ function About() {
   }
 
   return (
-    <div className="mt-20 mx-auto text-center max-w-4xl">
-      <h1 className="text-5xl font-bold mb-10">About {profile.name}</h1>
-      <div className="flex items-center justify-center mb-6 relative">
+    <div className="mt-20 text-center">
+      <div className="flex items-center justify-center mb-6 relative mx-64">
         <div className="avatar">
           <div className="ring-primary ring-offset-base-100 w-48 h-48 rounded-full ring ring-offset-2">
             <img
@@ -25,48 +24,39 @@ function About() {
         </div>
         <div className="ml-6 text-left">
           <h2 className="text-3xl font-bold mb-4">{profile.name}</h2>
-          <p className="text-xl mb-3">Job: {profile.job}</p>
-          <p className="text-xl">Country: {profile.country}</p>
+          <p className="text-xl mb-3">{profile.job}.{profile.college}</p>
+          <p className="text-xl">{profile.country}</p>
+          <p className="text-xl">{profile.mail}</p>
         </div>
       </div>
 
-      {/* for dropdown */}
-
-      <div
-        className="collapse bg-base-300"
-        style={{ maxWidth: "20rem", left: "20rem" }}
-      >
-        <input type="radio" name="my-accordion-1" defaultChecked />
-        <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
-        </div>
+      <div className="flex flex-row">
+      {/* Sort/Filter */}
+      <div className="flex flex-row mx-64">
+      <div>
+      <div className="collapse collapse-arrow bg-base-200">
+        <input type="radio" name="my-accordion-2" defaultChecked />
+        <div className="collapse-title text-xl font-medium">Sort</div>
         <div className="collapse-content">
           <p>hello</p>
         </div>
       </div>
-      <div
-        className="collapse bg-base-300"
-        style={{ maxWidth: "20rem", left: "20rem" }}
-      >
-        <input type="radio" name="my-accordion-1" />
-        <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
-        </div>
+      <div className="collapse collapse-arrow bg-base-200">
+        <input type="radio" name="my-accordion-2" />
+        <div className="collapse-title text-xl font-medium">Filter</div>
         <div className="collapse-content">
           <p>hello</p>
         </div>
       </div>
-      <div
-        className="collapse bg-base-300"
-        style={{ maxWidth: "20rem", left: "20rem" }}
-      >
-        <input type="radio" name="my-accordion-1" />
-        <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
+      </div>
+      </div>
+      <div>
+        <div className="bg-gray-200 p-5 max-w-auto">
+          <span>Title</span>
+          <span>Cited by</span>
+          <span>Year</span>
         </div>
-        <div className="collapse-content">
-          <p>hello</p>
-        </div>
+      </div>
       </div>
     </div>
   );
